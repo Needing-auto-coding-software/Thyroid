3871b1e5a6854c4548a4544c4f3ec601bc1f2050
yuaner <734172890@qq.com>
2021-01-24 22:46:50 +0800

    ReportCheckPage Layout

---
+++ app/src/main/java/com/example/thyroid/ReportCheckPage.java
@@ -0,0 +1,14 @@
+package com.example.thyroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportCheckPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_check_page);
+    }
+}
