7ea0e1fa095620b773595a3dd09c016ddeaaa50c
yuaner <734172890@qq.com>
2021-01-24 22:34:58 +0800

    PersonalPageActivity Layout

---
+++ app/src/main/java/com/example/thyroid/PersonalPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.thyroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_page);
+    }
+}
