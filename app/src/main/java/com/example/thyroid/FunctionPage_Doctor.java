c918013bfaef620802e0911f591db0af122287b9
yuaner <734172890@qq.com>
2021-01-26 11:58:54 +0800

    Add two functional pages, rename ids.

---
+++ app/src/main/java/com/example/thyroid/FunctionPage_Doctor.java
@@ -0,0 +1,14 @@
+package com.example.thyroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FunctionPage_Doctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_function_page__doctor);
+    }
+}
---
+++ app/src/main/java/com/example/thyroid/FunctionalPage_Patient.java
@@ -0,0 +1,14 @@
+package com.example.thyroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FunctionalPage_Patient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_functional_page__patient);
+    }
+}
